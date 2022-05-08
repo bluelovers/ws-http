f867372d6dcadba0cd0da0b0f524b7c9f8739bec
bluelovers <codelovers@users.sourceforge.net>
2022-05-08 10:08:16 +0800

    build(release): build new release

--- packages/response-stream/dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./http-response-stream.cjs.production.min.js')
-} else {
-  module.exports = require('./http-response-stream.cjs.development.js')
-}
